d5cf86aab47468748d3f222697b1318fe04d7d08
Wellington <wellington_notgnillew@hotmail.com>
2020-11-06 23:42:12 -0300

    Resend

--- Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CORE.BackOffice.UI.MvcApplication" Language="C#" %>
--- Scripts/Paginas/General.js
+++
@@ -1,5 +0,0 @@
-﻿$(document).ready(function () {
-    $('body').on('hidden.bs.modal', '.modal', function () {
-        $(this).removeData('bs.modal').find(".modal-content").empty();
-    });
-});
